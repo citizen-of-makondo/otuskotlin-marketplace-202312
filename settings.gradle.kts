9b80dcf3b40f28e257a7e666c493bc364722a324
Aleksandr Ilinskii <aleksandr8693@gmail.com>
2024-01-09 17:10:12 +0100

    Add function and add test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "otuskotlin-marketplace-202312"
-
+include("m1l1-first")
